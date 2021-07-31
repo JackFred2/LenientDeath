d635bddc2e70d6c9cf86e11b85ef2c8c0a3cbbfc
Jack Pearce <jack@jackf.red>
2021-07-31 11:38:05 +0100

    Injection-based item check, should be much more compatible

--- src/main/java/red/jackf/lenientdeath/mixins/PlayerInventoryMixin.java
+++ src/main/java/red/jackf/lenientdeath/mixins/PlayerInventoryMixin.java
@@ -4,6 +4,7 @@ import net.minecraft.entity.player.PlayerInventory;
 import net.minecraft.item.ItemStack;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.ModifyVariable;
 import org.spongepowered.asm.mixin.injection.Redirect;
 import red.jackf.lenientdeath.LenientDeath;
 
@@ -13,8 +14,11 @@ public abstract class PlayerInventoryMixin {
     /**
      * Check if the item should be kept, either due to it being nothing (the vanilla check) or by being a safe item.
      */
-    @Redirect(method = "dropAll", at = @At(value = "INVOKE", target = "Lnet/minecraft/item/ItemStack;isEmpty()Z"))
-    public boolean lenientdeath$checkEmptyOrSafe(ItemStack itemStack) {
-        return itemStack.isEmpty() || LenientDeath.isSafe(itemStack.getItem());
+    @ModifyVariable(method = "dropAll", at=@At(value = "INVOKE_ASSIGN", target = "Ljava/util/List;get(I)Ljava/lang/Object;", shift = At.Shift.BY, by = 2), ordinal = 0)
+    private ItemStack lenientdeath$checkStackSafe(ItemStack original) {
+        if (LenientDeath.isSafe(original.getItem())) {
+            return ItemStack.EMPTY;
+        }
+        return original;
     }
 }
