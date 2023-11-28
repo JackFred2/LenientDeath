ed47e82cd1b3e18d49538cc114ef91c2d5ff06d0
Jack Pearce <jack@jackf.red>
2023-11-28 21:00:45 +0000

    [feat] regular and objectshare API

---
+++ src/main/java/red/jackf/lenientdeath/api/LenientDeathAPI.java
@@ -0,0 +1,53 @@
+package red.jackf.lenientdeath.api;
+
+import net.fabricmc.loader.api.FabricLoader;
+import net.minecraft.server.level.ServerPlayer;
+import net.minecraft.world.entity.item.ItemEntity;
+import net.minecraft.world.item.ItemStack;
+import org.jetbrains.annotations.Nullable;
+import red.jackf.lenientdeath.apiimpl.LenientDeathAPIImpl;
+
+/**
+ * <p>API methods for working with Lenient Death. All methods are also available in Fabric's {@link FabricLoader#getObjectShare()},
+ * in case you don't want to depend on Lenient Death directly.</p>
+ *
+ * <p>ObjectShare example:</p>
+ * <pre>
+ *     {@code
+ *          ServerPlayer player = ...
+ *          ItemEntity item = ...
+ *          if (FabricLoader.getInstance().getObjectShare().get("lenientdeath:markDeathItem") instanceof TriConsumer<?, ?, ?> markDeathItem) {
+ *              //noinspection unchecked
+ *              ((TriConsumer<ServerPlayer, ItemEntity, @Nullable Integer>) markDeathItem).accept(player, item, null);
+ *          }
+ *     }
+ * </pre>
+ */
+public interface LenientDeathAPI {
+    /**
+     * Access for Lenient Death's API.
+     */
+    LenientDeathAPI INSTANCE = LenientDeathAPIImpl.INSTANCE;
+
+    /**
+     * <p>Test whether an item will be kept upon death. This checks for item filters, including the random check, as well
+     * as the specified player's per-player setting.</p>
+     * <p>ObjectShare: "lenientdeath:shouldItemBePreserved" {@code (BiFunction<ServerPlayer, ItemStack, Boolean>)}</p>
+     *
+     * @param deadPlayer Player which the given item will drop from.
+     * @param item Item being tested for preservation.
+     * @return Whether the item would be kept upon death.
+     */
+    boolean shouldItemBePreserved(ServerPlayer deadPlayer, ItemStack item);
+
+    /**
+     * <p>Mark an item entity as having dropped from a dead player. Intended for use by mods which provide inventory extensions,
+     * by granting the marked item a glow, extended lifetimes, world resistances, and optionally the source slot.</p>
+     * <p>ObjectShare: "lenientdeath:handleDeathItem" {@code (TriConsumer<ServerPlayer, ItemEntity, @Nullable Integer>) }</p>
+     *
+     * @param deadPlayer Player that the item dropped from.
+     * @param entity ItemEntity that was created from death.
+     * @param sourceSlot Inventory slot that the item dropped from. Optional, used to move the item back to the original slot.
+     */
+    void markDeathItem(ServerPlayer deadPlayer, ItemEntity entity, @Nullable Integer sourceSlot);
+}
