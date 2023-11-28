9e5f86ebc74eded6fa4b00c043bc172ac32dfbda
Jack Pearce <jack@jackf.red>
2023-11-28 03:08:03 +0000

    [fix] handle equipped trinket items not being handled correctly

---
+++ src/main/java/red/jackf/lenientdeath/mixinutil/LDMixinPlugin.java
@@ -0,0 +1,48 @@
+package red.jackf.lenientdeath.mixinutil;
+
+import net.fabricmc.loader.api.FabricLoader;
+import org.objectweb.asm.tree.ClassNode;
+import org.spongepowered.asm.mixin.extensibility.IMixinConfigPlugin;
+import org.spongepowered.asm.mixin.extensibility.IMixinInfo;
+
+import java.util.List;
+import java.util.Set;
+
+public class LDMixinPlugin implements IMixinConfigPlugin {
+    protected static final String TRINKETS_ITEM_DROP = "red.jackf.lenientdeath.mixins.compat.LivingEntityTrinketsMixin";
+
+    @Override
+    public void onLoad(String mixinPackage) {
+    }
+
+    @Override
+    public String getRefMapperConfig() {
+        return null;
+    }
+
+    @Override
+    public boolean shouldApplyMixin(String targetClassName, String mixinClassName) {
+        if (mixinClassName.equals(TRINKETS_ITEM_DROP)) return FabricLoader.getInstance().isModLoaded("trinkets");
+        return true;
+    }
+
+    @Override
+    public void acceptTargets(Set<String> myTargets, Set<String> otherTargets) {
+
+    }
+
+    @Override
+    public List<String> getMixins() {
+        return null;
+    }
+
+    @Override
+    public void preApply(String targetClassName, ClassNode targetClass, String mixinClassName, IMixinInfo mixinInfo) {
+
+    }
+
+    @Override
+    public void postApply(String targetClassName, ClassNode targetClass, String mixinClassName, IMixinInfo mixinInfo) {
+
+    }
+}
